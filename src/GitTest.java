2184a1557bc3ec83269e67c1a4c9eadd13bfca52
user <user@172.16.1.78>
2017-03-29 14:59:34 +0200

    Hello again!

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,13 +1,9 @@
 
 public class GitTest {
 
-	public GitTest() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+			System.out.println("We are testing Git.");
 	}
 
 }
